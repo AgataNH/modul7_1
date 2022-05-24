08a7ac3a3ff17add98e612ef9b5552c21505e0b9
Agata Niedolistek <a.niedolistek@ibe.edu.pl>
2022-05-24 12:21:01 +0200

    Add NameLenght

--- wizytowki.py
+++ wizytowki.py
@@ -9,11 +9,20 @@ class Card:
        self.job = job
        self.email = email
 
+   def contact(self):
+       print('Kontaktuję się z {} {}, pracującym jako {}, email: {}'.format(self.first_name, self.last_name, self.job, self.email))
+
+   @property
+   def namelength(self):
+      return len(self.first_name + ' ' + self.last_name)
+
 card_list=[]
 
-for _ in range(6):
+for _ in range(5):
     person = Card(first_name = fake.first_name(), last_name = fake.last_name(), company = fake.company(), job = fake.job(), email = fake.email())
     card_list.append(person)
 
 for item in card_list:
     print(item.first_name, item.last_name, item.company, item.job, item.email, '\t')
+    print(item.namelength)
+
