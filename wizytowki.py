27d02a147cc0e4fe8f0e4aab2d0bc588461f29c1
Agata Niedolistek <a.niedolistek@ibe.edu.pl>
2022-05-24 22:14:32 +0200

    Change CreateContact

--- wizytowki.py
+++ wizytowki.py
@@ -8,7 +8,7 @@ class BaseCard:
        self.phone = phone
        self.email = email
 
-   def contact_1(self):
+   def contact(self):
        return f'Wybieram numer {self.phone} i dzownię do {self.first_name} {self.last_name}'
 
    @property
@@ -22,28 +22,20 @@ class BusinessCard(BaseCard):
        self.job = job
        self.office_phone = office_phone
 
-   def contact_2(self):
+   def contact(self):
        return f'Wybieram numer {self.office_phone} i dzownię do {self.first_name} {self.last_name}, pracującym jako {self.job}'
 
-   @property
-   def namelength(self):
-      return len(self.first_name + ' ' + self.last_name)
-
 def create_contacts(card_type, card_number):
-    Card_list =[]
+    card_list = []
     for _ in range(card_number):
-        person = BusinessCard(first_name = fake.first_name(), last_name = fake.last_name(), phone = fake.phone_number(), email = fake.email(), company = fake.company(), job = fake.job(), office_phone = fake.phone_number())
-        Card_list.append(person)
+        if card_type == 'Base':
+            card = BaseCard(first_name = fake.first_name(), last_name = fake.last_name(), phone = fake.phone_number(), email = fake.email())
+        elif card_type == 'Business':
+            card = BusinessCard(first_name = fake.first_name(), last_name = fake.last_name(), phone = fake.phone_number(), email = fake.email(), company = fake.company(), job = fake.job(), office_phone = fake.phone_number())
+        card_list.append(card)
+    return card_list
 
-    if card_type == 'Base':
-        for item in Card_list:
-            print(item.first_name, item.last_name, item.phone, item.email, '\t')
-            print(item.namelength)
-            print(item.contact_1())
-    if card_type == 'Business':
-        for item in Card_list:
-            print(item.first_name, item.last_name, item.company, item.job, item.office_phone, '\t')
-            print(item.namelength)
-            print(item.contact_2())
-
-create_contacts('Business', 2)
+for item in create_contacts('Business', 2):
+    print(item.first_name, item.last_name, item.company, item.job, item.office_phone)
+    print(item.namelength)
+    print(item.contact(), '\n')
